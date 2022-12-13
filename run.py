f8cb372f822ee67e80113d9963db0da325a7016f
ShemmyYo <rabiliamemo@gmail.com>
2022-12-13 19:24:47 +0000

    function collecting user imput as number of lines to bet and checking if input is a valid number

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+MAX_LINES = 3
+
 def deposit():
     while True:
-        amount = input("What's your deposit? €\n")
+        amount = input(f"What's your deposit? €\n")
         if amount.isdigit():
             amount = int(amount)
             if amount > 0:
@@ -8,12 +10,37 @@ def deposit():
             else:
                 print("Amount must be grater than 0")
         else:
+            print(f"'{amount}', you entered is invalid\n")
             print("Please enter a valid amount again")
-            print("Amount must be grater than 0")
+            print("Amount must be a number, and grater than 0")
 
     print("Thank  You!")
     print(f"Your deposit is: €{amount}")
     return amount
 
 
-deposit() 
+def get_number_of_lines(): 
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")?\n")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print(f"Enter a valid number of lines (between 1-{MAX_LINES})\n")
+        else:
+            print(f"'{lines}', you entered is invalid\n")
+            print("Please enter a valid number of lines")
+            print(f"Number of lines must be a number between 1 and {MAX_LINES}\n")
+
+    print("Thank  You!")
+    print(f"You bet {lines} lines\n")
+    return lines
+
+
+def main():
+    balance = deposit()  
+    lines = get_number_of_lines()
+
+
+main()
