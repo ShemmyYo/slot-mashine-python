b07cc1f25f33805442bcc015baddeb486bc44024
ShemmyYo <rabiliamemo@gmail.com>
2022-12-13 17:43:07 +0000

    function collecting user imput as deposit and checking if input is a valid number

--- run.py
+++ run.py
@@ -1,1 +1,19 @@
+def deposit():
+    while True:
+        amount = input("What's your deposit? €\n")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be grater than 0")
+        else:
+            print("Please enter a valid amount again")
+            print("Amount must be grater than 0")
 
+    print("Thank  You!")
+    print(f"Your deposit is: €{amount}")
+    return amount
+
+
+deposit() 
