6bcf7f54f74eec6a8865c8158da9ed8f6edcd7b9
ShemmyYo <rabiliamemo@gmail.com>
2022-12-15 17:25:47 +0000

    added welcome screen, logo and rules plus colours and format to commands

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 MAX_LINES = 3
 MAX_BET = 100
@@ -49,16 +50,16 @@ def print_slot_mashine(columns):
     """
     function printing slot mashine by transposing column to line
     """
-    print(f"\n-+-+-")
+    print(f"\033[1;34;40m\n>>+-------+><+-------+><+-------+<<")
     for row in range(len(columns[0])):
         
         for i, column in enumerate(columns):
             if i != len(columns) -1:
-                print(column[row], end="|")
+                print("\033[1;34;40m  | ", "\033[1;31;40m", column[row], end="\033[1;34;40m   |")
             else:
-                print(column[row], end="")
+                print("\033[1;34;40m  | ", "\033[1;31;40m", column[row], end="\033[1;34;40m   |")
 
-        print(f"\n-+-+-")
+        print(f"\n>>+-------+><+-------+><+-------+<<")
 
 
 def win_check(columns, lines, bet, values):
@@ -74,8 +75,9 @@ def win_check(columns, lines, bet, values):
             winnings += values[symbol] * bet
             winning_lines.append(line +1)
             print()
-            print("Congratulations You won!")
-            print(f"You have won €{winnings}! on line: ", *winning_lines)
+            print("\n\033[1;33;40m  Congratulations! You won!!!")
+            print("\033[1;33;40m*******************************")
+            print(f"\033[1;32;40m  You have won €{winnings}! on line: ", *winning_lines)
 
     return winnings, winning_lines
 
@@ -86,19 +88,19 @@ def deposit():
     if input is a valid number
     """
     while True:
-        amount = input(f"What's your deposit? €")
+        amount = input(f"\033[1;33;40m  What's your deposit? €")
         if amount.isdigit():
             amount = int(amount)
             if amount > 0:
                 break
             else:
-                print("Amount must be grater then 0")
+                print("  NOTE: Amount must be grater then 0")
         else:
-            print(f"NOTE: '{amount}', you entered is not accepted!\n")
-            print("Please enter a valid amount again")
-            print("Amount must be a number, and grater the1000n 0")
+            print(f"\033[1;31;40m  NOTE: '{amount}', you entered is not accepted!\n")
+            print("\033[1;35;40m  Please enter a valid amount again")
+            print("\033[1;35;40m  Amount must be a number, and grater then 0")
 
-    print(f"Your deposit is: €{amount}\n")
+    print(f"\n\033[1;34;40m****************************************************************")
     return amount
 
 
@@ -108,19 +110,19 @@ def get_number_of_lines():
     checking if input is a valid number
     """
     while True:
-        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        lines = input("\033[1;33;40m  Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
         if lines.isdigit():
             lines = int(lines)
             if 1 <= lines <= MAX_LINES:
                 break
             else:
-                print(f"Enter a valid number of lines (between 1-{MAX_LINES})\n")
+                print(f"  Enter a valid number of lines (between 1-{MAX_LINES})\n")
         else:
-            print(f"NOTE: '{lines}', you entered is not accepted!\n")
-            print("Please enter a valid number of lines")
-            print(f"Number of lines must be a number between 1 and {MAX_LINES}\n")
+            print(f"\033[1;31;40m  NOTE: '{lines}', you entered is not accepted!\n")
+            print("\033[1;35;40m  Please enter a valid number of lines")
+            print(f"\033[1;35;40m  Number of lines must be a number between 1 and {MAX_LINES}\n")
 
-    print(f"You bet on {lines} lines\n")
+    print(f"\033[1;32;40m  >>>  You bet on {lines} lines\n")
     return lines
 
 
@@ -130,19 +132,21 @@ def get_bet():
     and checking if input is a valid number
     """
     while True:
-        bet = input("How much would you like to bet on each line? €")
+        bet = input("\033[1;33;40m  How much would you like to bet on each line? €")
         if bet.isdigit():
             bet = int(bet)
             if MIN_BET <= bet <= MAX_BET:
                 break
             else:
-                print(f"Bet amount must be between then €{MIN_BET} and €{MAX_BET}\n")
+                print(f"\033[1;31;40m  NOTE: Bet amount must be between then €{MIN_BET} and €{MAX_BET}\n")
         else:
-            print(f"NOTE: '{bet}', you entered is not correct!\n")
-            print("Please enter a valid bet amount again")
-            print(f"Bet amount must be a number grater then €{MIN_BET} and €{MAX_BET}\n")
+            print(f"\033[1;31;40m  NOTE: '{bet}', you entered is not correct!\n")
+            print("\033[1;35;40m  Please enter a valid bet amount again")
+            print(f"\033[1;35;40m  Bet amount must be a number grater then €{MIN_BET} and €{MAX_BET}\n")
 
-    print(f"Your bet amount is: €{bet}\n")
+    print(f"\033[1;32;40m  >>> Your bet amount is: €{bet}\n")
+    time.sleep(1)
+
     return bet
 
 
@@ -158,12 +162,12 @@ def spin(balance):
         total_bet = bet * lines
 
         if total_bet >= balance:
-            print(f"Insufficient balance! Your current balance is €{balance}!\n")
+            print(f"\033[1;31;40m  Insufficient balance! Your current balance is €{balance}!\n")
         else:
             break
 
-    print(f"You are betting €{bet} on {lines} lines")
-    print(f"Total bet is €{total_bet}")
+    print(f"\033[1;35;40m  You are betting €{bet} on {lines} lines")
+    print(f"\033[1;35;40m  Total bet is €{total_bet}")
 
     reels = spin_mashine(ROWS, COLS, symbol_count)
     print_slot_mashine(reels)
@@ -177,15 +181,42 @@ def main():
     """
     balance = deposit()  
     while True:
-        print(f"Current balance is €{balance}\n")
-        answer = input("Press Enter to spin (Q to Quit)")
+        print(f"\n\033[1;32;40m  Current balance is €{balance}! Lets play!\n")
+        answer = input("\033[1;35;40m  Press Enter to spin (Q to Quit)\n")
         if answer == "q":
             break
         balance += spin(balance)
 
-    print(f"Your balance is {balance}\n")
-    print("Good-bye!")
+    print(f"\033[1;32;40m  Your balance is €{balance}\n")
+    time.sleep(0.5)
+    print("\033[1;35;40m  Good-bye!")
 
+
+# Welcome logo
+print("\033[1;33;40m****************************************************************")
+print("\033[1;34;10m                 Welcome to One-Armed Bandit!")
+print("\033[1;33;40m****************************************************************\n")
+print("\033[1;34;10m   ####  ##   ## #####      ####  #####  ##   ## ##### #####")
+print("\033[1;35;10m  ##  ## ###  ## ##        ##  ## ##  ## ### ### ##    ##  ##")
+print("\033[1;36;10m  ##  ## ## #### ####  ##  ###### #####  ## # ## ####  ##  ##")
+print("\033[1;37;10m  ##  ## ##  ### ##        ##  ## ## ##  ##   ## ##    ##  ##")
+print("\033[1;38;10m   ####  ##   ## #####     ##  ## ##  ## ##   ## ##### #####\n")
+print()
+print("\033[1;38;10m        ####     ####   ##   ##  #####   ##  ######   ##")
+print("\033[1;37;10m        ##  ##  ##  ##  ###  ##  ##  ##  ##    ##     ##")
+print("\033[1;36;10m        #####   ######  ## ####  ##  ##  ##    ##     ##")
+print("\033[1;35;10m        ##  ##  ##  ##  ##  ###  ##  ##  ##    ##")
+print("\033[1;34;10m        ####    ##  ##  ##   ##  #####   ##    ##     ##\n")
+print("\033[1;33;40m****************************************************************")
+print("\033[1;31;40m                        by ShemmyYo")
+print("\033[1;33;40m****************************************************************\n")
+
+
+# Program description, functionality explanation
+print("\033[1;35;10m  The aim of the game is to make sure that all of the symbols")
+print("\033[1;35;10m  on the screen of the machine match up - get them all,")
+print("\033[1;35;10m  and you'll win the money.")
+print("\033[1;35;10m  All you need is a bag of change and you're ready to go!\n")
 
 
 main()
