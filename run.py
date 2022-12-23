0fa6218416ca0394f4ed97635d15c9056acb44d3
ShemmyYo <rabiliamemo@gmail.com>
2022-12-23 20:52:06 +0000

    final correction of some line spacing

--- run.py
+++ run.py
@@ -64,12 +64,12 @@ def player_details():
     player_info()
     while True:
         player_name = input("\033[1;33;40m  >>>  What is your name Player? \
-                             >>> ").capitalize()
+>>> ").capitalize()
         if player_name.isalpha():
             place = False
             while not place:
                 player_place = input("\033[1;33;40m  >>>  Which city \
-                                      are you from? >>> ").capitalize()
+are you from? >>> ").capitalize()
                 if player_place.isalpha():
                     player = Player(name=player_name, place=player_place,
                                     rounds=1, balance=DEPOSIT, wins=0, win=0)
@@ -90,16 +90,16 @@ def deposit(player):
     deposit_info()
     print(f"\033[1;33;40m  {player.name}, your default deposit is €100")
     print("\n\033[1;31;40m  >>> Press 'C' to change deposit/difficulty level")
-    answer = input("\n\033[1;35;10m  >>> Press Enter to continue").upper()
+    answer = input("\n\033[1;35;10m  >>> Press Enter to continue ").upper()
     if answer == "C":
         print(f"\n\033[1;33;40m  {player.name}, lets's get your \
-        deposit set!\n")
+deposit set!\n")
         print("\033[1;37;10m  >>>  Press '1' to set it to €50 (hard)")
         print("\033[1;37;10m  >>>  Press '2' to set it tt €100 (normal)")
         print("\033[1;37;10m  >>>  Press '3' to set it to €200 (easy)")
         while True:
             selection = input(f"\n\033[1;33;40m  {player.name}, what's your\
-                                decision? ").upper()
+ decision? ").upper()
             if selection == "1":
                 amount = 50
                 break
@@ -111,7 +111,7 @@ def deposit(player):
                 break
             else:
                 print(f"\n\033[1;31;40m  !!!  NOTE: '{selection}', you \
-                        entered is not accepted!\n")
+entered is not accepted!\n")
                 print("\033[1;31;40m  Choose from the above options!")
 
     else:
@@ -127,20 +127,20 @@ def get_number_of_lines():
     get_number_of_lines_info()
     while True:
         lines = input("\033[1;33;40m  Enter the number of lines to bet on \
-                       (1-" + str(MAX_LINES) + ")?  >>> ")
+(1-" + str(MAX_LINES) + ")?  >>> ")
         if lines.isdigit():
             lines = int(lines)
             if 1 <= lines <= MAX_LINES:
                 break
             else:
                 print(f"\033[1;31;40m  !!!  NOTE: Enter a valid number of \
-                        lines (between 1-{MAX_LINES})\n")
+lines (between 1-{MAX_LINES})\n")
         else:
             print(f"\033[1;31;40m  !!!  NOTE: '{lines}', you entered is \
-                    not accepted!\n")
+not accepted!\n")
             print("\033[1;35;40m  Please enter a valid number of lines")
             print(f"\033[1;35;40m  Number of lines must be a number between\
-                     1 and {MAX_LINES}\n")
+ 1 and {MAX_LINES}\n")
     print(f"\033[1;32;40m  >>>  You bet on {lines} lines")
     return lines
 
@@ -153,7 +153,7 @@ def get_bet():
     get_bet_info()
     while True:
         bet = input("\033[1;33;40m  How much would you like to bet on each\
-                     line?  >>> €")
+ line?  >>> €")
         if bet.isdigit():
             bet = int(bet)
             if MIN_BET <= bet <= MAX_BET:
@@ -163,10 +163,10 @@ def get_bet():
                          €{MIN_BET} and €{MAX_BET}\n")
         else:
             print(f"\033[1;31;40m  !!!  NOTE: '{bet}', you entered is not \
-                    correct!\n")
+ correct!\n")
             print("\033[1;35;40m  Please enter a valid bet amount again")
             print(f"\033[1;35;40m  Bet amount must be a number between \
-                    €{MIN_BET} and €{MAX_BET}\n")
+€{MIN_BET} and €{MAX_BET}\n")
     print(f"\033[1;32;40m  >>> Your bet amount is: €{bet}\n")
     time.sleep(1)
     return bet
@@ -202,7 +202,7 @@ def spin(balance, player):
     spin_info(player)
     print("\033[1;34;40m>"*25 + " "*16 + "<"*25)
     print("\033[1;33;40m "*13 + f">>> You are betting €{bet} on \
-            {lines} line(s) <<<" + " "*9)
+{lines} line(s) <<<" + " "*9)
     print("\033[1;33;40m "*21 + f">>> Total bet is €{total_bet} <<<" + " "*22)
     print("\033[1;34;40m>"*25 + " "*16 + "<"*25)
     reels = spin_mashine(ROWS, COLS, symbol_count)
@@ -242,10 +242,10 @@ def print_slot_mashine(columns):
         for i, column in enumerate(columns):
             if i != len(columns)-1:
                 print("\033[1;33;40m      |  ", "\033[1;31;40m\
-                      ", column[row], end="\033[1;33;40m    |    ")
+", column[row], end="\033[1;33;40m    |    ")
             else:
                 print("\033[1;33;40m      |  ", "\033[1;31;40m\
-                        ", column[row], end="\033[1;33;40m    |       ")
+", column[row], end="\033[1;33;40m    |       ")
         print("\n\033[1;33;40m " + "  --<+>>>>X<<<<+>--  "*3 + "  ")
     print(" "*66)
 
@@ -271,13 +271,13 @@ def win_check(columns, lines, bet, values, player):
             player.wins += 1
             print()
             print("\n" + "\033[1;33;40m "*15 + f"Congratulations \
-                    {player.name}! You won!!!")
+{player.name}! You won!!!")
     print("\033[1;33;40m*"*66)
     print(f"\033[1;32;40m  >>>  {player.name}, you have won €{winnings}! \
-            on line(s): ", *winning_lines)
+on line(s): ", *winning_lines)
     print("\033[1;31;40mx"*66)
     print(f"\033[1;31;40m  >>>  {player.name}, you have lost on line(s): \
-            ", *loosing_lines)
+", *loosing_lines)
     return winnings, winning_lines
 
 
@@ -288,7 +288,7 @@ def update_highscores(player):
     for count, rounds in enumerate(highscores_rounds[1:11], 2):
         if player.rounds > int(rounds[2]):
             print(f"\033[1;33;40m  >>>  Well done {player.name}, you made \
-                    the top 10!\n")
+the top 10!\n")
             player_as_list = [player.name, player.place, player.rounds,
                               player.wins, player.win]
             highscores.append_row(player_as_list)
@@ -297,7 +297,7 @@ def update_highscores(player):
             break
     else:
         print(f"\033[1;31;40m  >>>  No can do {player.name}, \
-                You didn't make the top 10\n")
+You didn't make the top 10\n")
 
 
 def display_highscores():
@@ -374,24 +374,20 @@ def main():
     if balance > 0:
         while True:
             clear_screen()
+            print("\033[1;33;40m "*20 + f"+++  {player.name}, Round: \
+{player.rounds}  +++")
+            print("\033[1;34;40m>"*27 + " "*11 + "<"*28)
+            print("\033[1;32;40m "*17 + f">>>  Current balance is \
+€{balance}! <<<" + " "*16)
             if balance <= 0:
                 game_over_info(player)
                 main()
             else:
-                print("\033[1;33;40m "*20 + f"+++  \
-                        {player.name}, Round: {player.rounds}  +++")
-                print("\033[1;33;40m "*22 + f"+++  \
-                        You have won {player.wins} round(s)!  +++")
-                print("\033[1;33;40m "*28 + f"+++  \
-                        Total of €{player.win}  +++")
-                print("\033[1;34;40m>"*27 + " "*11 + "<"*28)
-                print("\033[1;32;40m "*17 + f">>>  Current balance is \
-                        €{balance}! <<<" + " "*16)
                 print("\033[1;34;40m "*23 + ">>>  Let's play! <<<" + " "*23)
                 print("\033[1;34;40m>"*27 + " "*11 + "<"*28)
                 answer = input("\033[1;31;40m>"*13 + "  Press >>> Enter t\
-                        o Spin <<< (E for Exit)  " + "<"*9).upper()
-                if answer == "E":
+o Spin <<< (M for MENU)  " + "<"*9).upper()
+                if answer == "M":
                     game_over_info(player)
                     time.sleep(2)
                     main()
@@ -498,29 +494,28 @@ def intructions(player):
     print("\033[1;34;40m>"*25 + " "*14 + "<"*27)
     print("\033[1;33;40m-"*66)
     print("\033[1;35;10m  The aim of the game is to make sure that all of the\
-            symbols     ")
+ symbols     ")
     print("\033[1;35;10m  on the screen of the machine match up in line" + "\
-            "*19)
+ "*19)
     print("\033[1;35;10m  Get them all, and you'll win the money!" + " "*25)
     print("\033[1;33;40m="*66)
     print("\033[1;35;10m  1. Only horizontal 1 to 3 lines can be bet on" + "\
-            "*19)
+ "*19)
     print("\033[1;35;10m  2. When betting on lines you start betting on top \
-            line first -1-")
-    print("\033[1;35;10m     middle second -2- and bottom third -3-" + " "*22)
+line first -1-")
+    print("\033[1;35;10m     middle -2- second and bottom -3- third." + " "*22)
     print("\033[1;35;10m  3. There is: 3 x ♥, 4 x ♦, 6 x ♠ and 7 x ♣ symbols  \
             ")
-    print("\033[1;35;10m  4. Valued at :  ♥ x 5, ♦ x 4, ♠ x 3 and ♣ x 2       \
+    print("\033[1;35;10m  4. Valued at :  ♥ - 5, ♦ - 4, ♠ - 3 and ♣ - 2       \
             ")
     print("\033[1;35;10m  5. Min. bet is €1 and max is €100" + " "*31)
-    print("\033[1;35;10m  6. Up/Down your deposit to change difficulty level")
     print("\033[1;33;40m="*66)
     print(f"\033[1;35;10m  {player.name},")
     print("\033[1;35;10m  all you need is a bag of change and you're ready \
-            to go!         ")
+to go!         ")
     print("\033[1;33;40m-"*66)
     answer = input("\033[1;33;40m  >>>  Press Enter to go to >>> MAIN MENU <<<\
-                     (Q to Quit)  <<<    \n").upper()
+ (Q to Quit)  <<<    \n").upper()
     if answer == "Q":
         quit()
 
@@ -558,7 +553,7 @@ def get_bet_info():
 def spin_info(player):
     print("\033[1;34;40m>"*25 + " "*16 + "<"*25)
     print("\033[1;35;10m "*20 + f">>>  Turn {player.rounds} Results  \
-          <<<" + " "*20)
+<<<" + " "*20)
     print("\033[1;34;40m>"*25 + " "*16 + "<"*25)
 
 
@@ -571,7 +566,7 @@ def highscores_info():
 def game_over_info(player):
     print("\033[1;34;40m>"*27 + " "*11 + "<"*28)
     print("\033[1;33;40m "*12 + f">>> {player.name} you played \
-          {player.rounds} round(s)! <<<")
+{player.rounds} round(s)! <<<")
     print("\033[1;35;10m "*15 + f">>> You won {player.wins} time(s)! <<<")
     print("\033[1;35;10m "*18 + f">>> Total win of €{player.win}! <<<")
     print()
@@ -592,7 +587,7 @@ def game_over_info(player):
     print("\033[1;35;10m>"*33 + "<"*33)
     print(f"\n\033[1;35;10m  >>>  Thanks for playing {player.name}!\n")
     answer = input("\033[1;33;40m  >>>  Press Enter to go to >>> MAIN MENU <<<\
-                    (Q to Quit)  <<<    \n").upper()
+ (Q to Quit)  <<<    \n").upper()
     if answer == "Q":
         quit()
 
